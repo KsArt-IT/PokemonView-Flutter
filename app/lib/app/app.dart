7dc972371dbf934f1ea559594072750ac55b30bf
KsArT-IT <ksart.it@gmail.com>
2025-08-13 23:44:03 +0300

    feat: add main app class with MaterialApp settings and home page

---
+++ app/lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_view/presentation/page/home/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokemon View',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent)),
+      home: const HomePage(),
+    );
+  }
+}
