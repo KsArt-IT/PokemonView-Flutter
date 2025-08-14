26e5c839f86dbbe842906ee02ed4bc0753502479
KsArT-IT <ksart.it@gmail.com>
2025-08-14 23:09:10 +0300

    feat: add PokemonItem

---
+++ app/lib/data/models/pokemon_item.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'pokemon_item.freezed.dart';
+part 'pokemon_item.g.dart';
+
+@freezed
+class PokemonItem with _$PokemonItem {
+  const factory PokemonItem({
+    required String name,
+    required String url, //
+  }) = _PokemonItem;
+
+  factory PokemonItem.fromJson(Map<String, dynamic> json) => _$PokemonItemFromJson(json);
+}
