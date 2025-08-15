4ba3b658afb9d7c48182ccd9968e23c8d5731ebb
KsArT-IT <ksart.it@gmail.com>
2025-08-15 23:01:21 +0300

    add NetworkManager

---
+++ app/lib/data/provider/network_manager.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'network_manager.g.dart';
+
+@riverpod
+class NetworkManager extends _$NetworkManager {
+  static const _baseUrl = 'https://pokeapi.co/api/v2/';
+  static const _connectTimeout = Duration(seconds: 10);
+  static const _receiveTimeout = Duration(seconds: 10);
+
+  @override
+  FutureOr<Dio> build() async {
+    final dio = Dio(BaseOptions(
+      baseUrl: _baseUrl,
+      connectTimeout: _connectTimeout,
+      receiveTimeout: _receiveTimeout,
+    ));
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        print('Request: ${options.uri}');
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        print('Response: ${response.statusCode}');
+        return handler.next(response);
+      },
+      onError: (error, handler) {
+        print('Error: ${error.response?.statusCode}');
+        return handler.next(error);
+      },
+    ));
+    return dio;
+  }
+}
