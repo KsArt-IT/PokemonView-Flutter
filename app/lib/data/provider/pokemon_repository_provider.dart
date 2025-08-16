5a56946324763dd40a9f34a4a0f66c5e60adaf64
KsArT-IT <ksart.it@gmail.com>
2025-08-16 16:54:00 +0300

    change PokemonRepository provider

--- app/lib/data/provider/pokemon_repository_provider.dart
+++ app/lib/data/provider/pokemon_repository_provider.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pokemon_view/data/repository/pokemon_repository_impl.dart';
 import 'package:pokemon_view/data/provider/network_manager.dart';
+import 'package:pokemon_view/data/repository/pokemon_repository_impl.dart';
 import 'package:pokemon_view/domain/repository/pokemon_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'pokemon_repository_provider.g.dart';
 
 @riverpod
-Future<PokemonRepository> pokemonRepository(Ref ref) async {
-  final dio = await ref.read(networkManagerProvider.future);
-  return PokemonRepositoryImpl(dio: dio);
+PokemonRepository pokemonRepository(Ref ref) {
+  return PokemonRepositoryImpl(dio: ref.read(networkManagerProvider));
 }
